7f782b3e1c451a4f1007a4f7fb8ca385d0c99fbd
xaraf <nu01280@gmail.com>
2023-08-07 00:12:16 +0100

    adding dist subtree to deploy in github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-task-manager/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
